434ab6aae920aeab02e77d8ebabb3cd5a8eed4e4
qmehdi <qmehdi110@gmail.com>
2021-02-23 12:19:16 -0600

    Added comments for employee_skill table and enums

--- critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Employee.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/entity/Employee.java
@@ -24,13 +24,28 @@ public class Employee
     private long id;
     private String name;
 
+    /**
+     * id | skill
+     * 1  | WALKING
+     * 1 | Feeding
+     * 2 | WALKING
+     * This table will contain only unique values for skills so there can't be another row like 1 | Feeding
+     */
+    // Whenever we use an enum (<EmployeeSkill>), we need to add the @ElementCollection annotation
     @ElementCollection
+    // Create a new table called `employee_skill` and for each employee ID there will be a list of employee skills.
     @CollectionTable(
             name="employee_skill",
             joinColumns = @JoinColumn(name="id"), uniqueConstraints = @UniqueConstraint(columnNames = {"ID", "SKILL"}))
     @Column(name="skill")
     private Set<EmployeeSkill> skills;
 
+    /**
+     *  id | day
+     *  1  | SUNDAY
+     *  1  | MONDAY
+     *  2  | SUNDAY
+     */
     @ElementCollection
     @CollectionTable(
             name="day_of_week",
--- critter/src/main/java/com/udacity/jdnd/course3/critter/entity/EmployeeSkill.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/entity/EmployeeSkill.java
@@ -3,6 +3,9 @@ package com.udacity.jdnd.course3.critter.entity;
 /**
  * A example list of employee skills that could be included on an employee or a schedule request.
  */
+// This is just an enum and at this point, this is not a table in the database. The table is created in the Employee entity
+// It's a class containing some constants (constants could be strings and integers.
+// These values can't be changed at runtime; for example, to add a new skill called sleeping, you would have to modify this class here.
 public enum EmployeeSkill {
     PETTING, WALKING, FEEDING, MEDICATING, SHAVING;
 }
